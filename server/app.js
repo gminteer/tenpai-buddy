e36040cd07de9facaaecc69980d4c726cead293b
gminteer <gminteer@gmail.com>
2020-11-12 04:52:01 -0500

    maybe some shenanigans will fix it?

--- server/app.js
+++ server/app.js
@@ -17,9 +17,11 @@ apolloServer.applyMiddleware({app});
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production')
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
+}
 
 module.exports = app;
