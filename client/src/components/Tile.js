3ff1f369bbfcf44dcfcaaa80b3eecd09a9dddcb8
gminteer <gminteer@gmail.com>
2020-11-12 04:10:08 -0500

    game over submits score

--- client/src/components/Tile.js
+++ client/src/components/Tile.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-import {faces} from 'assets/tiles';
+import {faces, back} from 'assets/tiles';
 
+import {tile} from 'utils/mahjong/helper';
 import styles from './Tile.module.scss';
 
 export default function Tile({id, clickHandler}) {
   return (
     <div className={styles.Tile} onClick={clickHandler}>
-      <img src={faces[id % 40]} />
+      <img
+        src={id === 'back' ? back : faces[id % 40]}
+        alt={tile.toString(id)}
+      />
     </div>
   );
 }
