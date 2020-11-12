49c039205f45526f676d73ad60b1b62668ab0ec7
gminteer <gminteer@gmail.com>
2020-11-12 07:28:57 -0500

    sanity check

--- server/app.js
+++ server/app.js
@@ -19,12 +19,12 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('/service-worker.js', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/src/serviceWorker.js'));
-  });
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
+  // app.get('/service-worker.js', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../client/src/serviceWorker.js'));
+  // });
+  // app.get('/*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // });
 }
 
 module.exports = app;
