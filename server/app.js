d9510b579d55cd9efbd54a1092251c98a5f307f7
gminteer <gminteer@gmail.com>
2020-11-12 04:49:01 -0500

    do i need some react router shenanigans?

--- server/app.js
+++ server/app.js
@@ -18,6 +18,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production')
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
 module.exports = app;
