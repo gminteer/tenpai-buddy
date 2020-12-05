3777589b1c5aea6f102e17003b7ae1fb55f88fd3
gminteer <gminteer@gmail.com>
2020-12-04 20:59:08 -0500

    forgot to .gitignore dist

--- server/dist/models/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Score = exports.Profile = exports.Account = void 0;
-var Account_1 = require("./Account");
-Object.defineProperty(exports, "Account", { enumerable: true, get: function () { return Account_1.Account; } });
-var Profile_1 = require("./Profile");
-Object.defineProperty(exports, "Profile", { enumerable: true, get: function () { return Profile_1.Profile; } });
-var Score_1 = require("./Score");
-Object.defineProperty(exports, "Score", { enumerable: true, get: function () { return Score_1.Score; } });
